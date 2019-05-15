1572f3a430f88ba94138e931faefa9d73584f0ea
iamvaibhavkr <vaibhav.169105211@muj.manipal.edu>
2019-05-15 14:21:39 +0530

    Final upload

--- Tester.py
+++ Tester.py
@@ -9,7 +9,7 @@ prediction.setModelPath("idenprof_061-0.7933.h5")
 prediction.setJsonPath("idenprof_model_class.json")
 prediction.loadModel(num_objects=10)
 
-predictions, probabilities = prediction.predictImage("test-images\\firefighter-4.jpg", result_count=3)
+predictions, probabilities = prediction.predictImage("test-images\\test.jpg", result_count=3)
 
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
